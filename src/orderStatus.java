ba925b277af5e95d19518e4ae8aee02849c76f9b
Maja <Leoka@users.noreply.github.com>
2016-03-24 14:08:09 +0100

    Update orderStatus.java
    - add next() function

--- src/orderStatus.java
+++ src/orderStatus.java
@@ -2,5 +2,9 @@
  * Created by emajgra on 3/23/2016.
  */
 public enum orderStatus {
-    PENDING, COMPLETED,PREPARATION_IN_PROGRESS, IN_SHIPPING, DELIVERED
-}
+    PENDING, COMPLETED, PREPARATION_IN_PROGRESS, IN_SHIPPING, DELIVERED;
+
+    public orderStatus next() {
+        return values()[ordinal() + 1];
+    }
+    }
